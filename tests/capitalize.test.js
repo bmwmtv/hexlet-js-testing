6b2ead947dd2ed31cd13221c9a1297a8e1414c69
bmwmtv <bmwmtv@gmail.com>
2022-07-22 18:16:54 +0700

    update 2

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,14 @@
+  import { strict as assert } from 'assert';
+// при использовании strict-режима
+// проверка equal равносильна strictEqual
+
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Функция работает неверно!');
-  }
-  
-  if (capitalize('') !== '') {
-    throw new Error('Функция работает неверно!');
-  }
-  
-  console.log('Все тесты пройдены!');
+// Проверка сменилась с отрицательной на положительную
+assert.equal(capitalize(''), '');
+// Первый параметр actual – то, что пришло
+// Второй параметр expected – то, что ожидает тест
+// Правильный порядок аргументов имеет большое значение при анализе ошибки
+assert.equal(capitalize('hello'), 'Hello');
+
+console.log('ok');
